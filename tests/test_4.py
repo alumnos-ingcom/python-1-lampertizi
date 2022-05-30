21f8e8c52e18bd9a3fa17985d6177b0f44fc5431
lampertizi <tizianolamperti@gmail.com>
2022-05-30 18:57:16 -0300

    arreglados los resultados esperados en los assert

--- tests/test_4.py
+++ tests/test_4.py
@@ -11,7 +11,7 @@ def test_nombrefuncion():
     """
     se ingresarán valores tanto positivos como negativos y cero
     """
-    assert suma_lenta(5,3) == "=8", "error en la suma"
-    assert suma_lenta(4,-2) == "=2", "error en la resta"
-    assert suma_lenta(9,0) == "=9", "error cuando se suma cero"
+    assert suma_lenta(5,3) == "= 8", "error en la suma"
+    assert suma_lenta(4,-2) == "= 2", "error en la resta"
+    assert suma_lenta(9,0) == "= 9", "error cuando se suma cero"
     assert suma_lenta(0,-3) == "= -3", "error cuando se le resta a cero"
