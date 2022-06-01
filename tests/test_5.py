aec18f8618d9dea7eb8c315b655d25c77c4c3f5e
lampertizi <tizianolamperti@gmail.com>
2022-05-31 23:31:11 -0300

    descomentada la linea que importa modulos

--- tests/test_5.py
+++ tests/test_5.py
@@ -2,7 +2,7 @@
 # Tiziano Lamperti - @lampertizi
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-#from src.ejercicio5 import division_lenta
+from src.ejercicio5 import division_lenta
 """
 se probará la función que hace una division de a pasos / lenta
 """
@@ -15,5 +15,3 @@ def test_division_lenta():
     assert division_lenta(18,5) == (3,3), "error en la division con resto != de cero" 
     assert division_lenta(15,0) == ZeroDivisionError,"puede dividir por cero O_o"
     assert division_lenta(-10,2) == (-5,0),"error en la división de n° negativos"
-    
-
