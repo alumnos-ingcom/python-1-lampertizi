02943d7a227c9732f2234a1d6b177e26a5187e52
lampertizi <tizianolamperti@gmail.com>
2022-06-01 02:05:19 -0300

    optimizados casos de prueba y embellecido para el robot

--- tests/test_11.py
+++ tests/test_11.py
@@ -12,9 +12,7 @@ def test_multiplos():
     """
     se insertarán n° negativos, positivos, y neutros
     """
-    
-    assert es_multiplo(15,2) == False,"no detecta cuando no es multiplo"
-    assert es_multiplo(98,2) == True, "no detecta cuando si es multiplo"
-    assert es_multiplo(-40,5) == True, "no detecta cuando hay n° negativos"
-    assert es_multiplo(18,-2) == True, "no detecta cuando hay n° negativos"
-    assert es_multiplo(78,0) == AssertionError, "no frena la división por cero"
+    assert es_multiplo(15,2) is False,"no detecta cuando no es multiplo"
+    assert es_multiplo(98,2) is True, "no detecta cuando si es multiplo"
+    assert es_multiplo(-40,5) is True, "no detecta cuando hay n° negativos"
+    assert es_multiplo(18,-2) is True, "no detecta cuando hay n° negativos"
