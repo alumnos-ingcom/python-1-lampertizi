7f210b5c90744af9670c524db4d4d552c147571c
lampertizi <tizianolamperti@gmail.com>
2022-06-01 01:59:07 -0300

    optimizado el caso de prueba de n° repetiso

--- tests/test_6.py
+++ tests/test_6.py
@@ -10,9 +10,9 @@ se probarán las funciones que ordenan de mayor a menor y viceversa
 
 def test_menormayor():
     """
-    se evaluarán valores neutros, repetidos y negativos 
+    se evaluarán valores neutros, repetidos y negativos
     """
-    caso_negativos = menor_a_mayor(-12,-65,-19) 
+    caso_negativos = menor_a_mayor(-12,-65,-19)
     caso_neutros = menor_a_mayor(0,9,6)
     caso_repetidos = menor_a_mayor(7,7,90)
 
@@ -23,14 +23,13 @@ def test_menormayor():
 
 def test_mayormenor():
     """
-    se evaluarán valores neutros, repetidos y negativos 
+    se evaluarán valores neutros, repetidos y negativos
     """
 
-    caso_negativos = mayor_a_menor(-12,-65,-19) 
+    caso_negativos = mayor_a_menor(-12,-65,-19)
     caso_neutros = mayor_a_menor(0,9,6)
     caso_repetidos = mayor_a_menor(7,7,90)
 
     assert caso_negativos == (-12, -19, -65), "no ordena valores negativos"
     assert caso_neutros == (9, 6, 0), "no ordena con valor neutro"
     assert caso_repetidos == (90, 7, 7), "no ordena valores repetidos"
-
