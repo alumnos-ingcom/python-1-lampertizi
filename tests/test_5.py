63fd157ffea690bb3f9245a8daa6d8c3bbc5fd22
lampertizi <tizianolamperti@gmail.com>
2022-06-01 01:58:35 -0300

    agregados casos de prueba

--- tests/test_5.py
+++ tests/test_5.py
@@ -9,8 +9,9 @@ se probará la función que hace una division de a pasos / lenta
 
 def test_division_lenta():
     """
-    se probarán valores negativos, positivos y con cero
+    se probarán valores negativos y positivos
     """
     assert division_lenta(18,2) == (9,0),"error en la división con resto cero"
-    assert division_lenta(18,5) == (3,3), "error en la division con resto != de cero" 
-    assert division_lenta(15,0) == ZeroDivisionError,"puede dividir por cero O_o"
+    assert division_lenta(18,5) == (3,3), "error en la division con resto != de cero"
+    assert division_lenta(-78,2) == (-39,0), "no funciona con n° negativos"
+    assert division_lenta(78,-2) == (-39,0), "no funciona con n° negativos"
