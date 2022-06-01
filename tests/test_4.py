5bc0560f262f94e68afd098fe909a633ca95d310
lampertizi <tizianolamperti@gmail.com>
2022-05-31 23:30:00 -0300

    arreglados los casos de prueba

--- tests/test_4.py
+++ tests/test_4.py
@@ -11,7 +11,12 @@ def test_nombrefuncion():
     """
     se ingresarán valores tanto positivos como negativos y cero
     """
-    assert suma_lenta(5,3) == "= 8", "error en la suma"
-    assert suma_lenta(4,-2) == "= 2", "error en la resta"
-    assert suma_lenta(9,0) == "= 9", "error cuando se suma cero"
-    assert suma_lenta(0,-3) == "= -3", "error cuando se le resta a cero"
+    res1 = "5 + 1 + 1 + 1 = 8"
+    res2 = "4 - 1 - 1 = 2"
+    res3 = "9 + 0 = 9"
+    res4 = "0 - 1 - 1 - 1 = -3"
+
+    assert suma_lenta(5,3) == res1, "error en la suma"
+    assert suma_lenta(4,-2) == res2, "error en la resta"
+    assert suma_lenta(9,0) == res3,"error cuando se suma cero"
+    assert suma_lenta(0,-3) == res4, "error cuando se le resta a cero"
